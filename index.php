62a7681670f85bf72d6f3e492f777ef6ec42b6e8
Marius Badanau <mbadanau@endava.net>
2016-05-17 10:45:02 +0300

    comit sourcetree

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// whether ip from share internet
+if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+	$ip_address = $_SERVER['HTTP_CLIENT_IP'];
+}
+// whether ip is from proxy
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+	$ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+// wheder ip is from remote address
+else {
+	$ip_address = $_SERVER['REMOTE_ADDR'];
+}
+echo $ip_address;
+
+
+?>
